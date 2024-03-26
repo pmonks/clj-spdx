8c992aba087afb151fd0d0232cbfd18a8fea7e2c
Peter Monks <pmonks@gmail.com>
2024-03-26 13:56:16 -0700

    :green_heart: Rename main branch to release

--- pbr.clj
+++ pbr.clj
@@ -22,6 +22,7 @@
   (assoc opts
          :lib          'com.github.pmonks/clj-spdx
          :version      (pbr/calculate-version 1 0)
+         :prod-branch  "release"
          :write-pom    true
          :validate-pom true
          :pom          {:description      "Clojure wrapper around spdx/Spdx-Java-Library."
