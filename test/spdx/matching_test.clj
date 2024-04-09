8c06d5e9fa93a30e7f9f4bc74df9615ab9e35ba3
Peter Monks <pmonks@gmail.com>
2024-04-09 15:57:25 -0700

    :umbrella: Comment out failing test due to www.wtfpl.net going away

--- test/spdx/matching_test.clj
+++ test/spdx/matching_test.clj
@@ -50,7 +50,7 @@
 (def cc-by-nd-40-text                (delay (slurp "https://creativecommons.org/licenses/by-nd/4.0/legalcode.txt")))
 (def cc-by-nc-nd-40-text             (delay (slurp "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode.txt")))
 
-(def wtfpl-text                      (delay (slurp "http://www.wtfpl.net/txt/copying/")))
+;(def wtfpl-text                      (delay (slurp "http://www.wtfpl.net/txt/copying/")))   ; Site stopped responding some time in 2024
 
 (def mpl-20-text                     (delay (slurp "https://www.mozilla.org/media/MPL/2.0/index.txt")))
 
@@ -107,7 +107,7 @@
 ;    (is (true?  (text-is-license? @cc-by-nc-sa-40-text "CC-BY-NC-SA-4.0")))  ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
 ;    (is (true?  (text-is-license? @cc-by-nd-40-text    "CC-BY-ND-4.0")))     ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
 ;    (is (true?  (text-is-license? @cc-by-nc-nd-40-text "CC-BY-NC-ND-4.0")))  ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
-    (is (true?  (text-is-license? @wtfpl-text          "WTFPL")))
+;    (is (true?  (text-is-license? @wtfpl-text          "WTFPL")))            ; Site stopped responding some time in 2024
     (is (true?  (text-is-license? @mpl-20-text         "MPL-2.0"))))
   (testing "Exactly matching 3rd party license texts"
     (is (true?  (text-is-license? @clj-spdx-license        "Apache-2.0")))
@@ -163,7 +163,7 @@
 ;    (is (true?  (text-contains-license? @cc-by-nc-sa-40-text "CC-BY-NC-SA-4.0")))  ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
 ;    (is (true?  (text-contains-license? @cc-by-nd-40-text    "CC-BY-ND-4.0")))     ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
 ;    (is (true?  (text-contains-license? @cc-by-nc-nd-40-text "CC-BY-NC-ND-4.0")))  ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
-    (is (true?  (text-contains-license? @wtfpl-text          "WTFPL")))
+;    (is (true?  (text-contains-license? @wtfpl-text          "WTFPL")))            ; Site stopped responding some time in 2024
     (is (true?  (text-contains-license? @mpl-20-text         "MPL-2.0"))))
   (testing "3rd party license text contains license"
     (is (true?  (text-contains-license? @clj-spdx-license        "Apache-2.0")))
@@ -253,7 +253,7 @@
 ;    (is (= (licenses-within-text @cc-by-nc-sa-40-text) #{"CC-BY-NC-SA-4.0"}))  ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
 ;    (is (= (licenses-within-text @cc-by-nd-40-text)    #{"CC-BY-ND-4.0"}))     ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
 ;    (is (= (licenses-within-text @cc-by-nc-nd-40-text) #{"CC-BY-NC-ND-4.0"}))  ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
-    (is (= (licenses-within-text @wtfpl-text)          #{"WTFPL"}))
+;    (is (= (licenses-within-text @wtfpl-text)          #{"WTFPL"}))            ; Site stopped responding some time in 2024
     (is (= (licenses-within-text @mpl-20-text)         #{"MPL-2.0-no-copyleft-exception" "MPL-2.0"})))
   (testing "Matching 3rd party license texts that only contain a single license"
     (is (= (licenses-within-text @clj-spdx-license)        #{"Apache-2.0"}))
@@ -263,7 +263,7 @@
     (is (= (licenses-within-text (str "ABCD\n" @gpl-30-text             "\nEFGH")) #{"GPL-3.0-only" "GPL-3.0+" "GPL-3.0-or-later" "GPL-3.0"}))
 ;    (is (= (licenses-within-text (str "ABCD\n" @cc-by-40-text           "\nEFGH")) #{"CC-BY-4.0"}))  ; Failing due to https://github.com/spdx/Spdx-Java-Library/issues/233
     (is (= (licenses-within-text (str "ABCD\n" @mpl-20-text             "\nEFGH")) #{"MPL-2.0-no-copyleft-exception" "MPL-2.0"}))
-    (is (= (licenses-within-text (str "ABCD\n" @wtfpl-text              "\nEFGH")) #{"WTFPL"}))
+;    (is (= (licenses-within-text (str "ABCD\n" @wtfpl-text              "\nEFGH")) #{"WTFPL"}))      ; Site stopped responding some time in 2024
     (is (= (licenses-within-text (str "ABCD\n" @clj-spdx-license        "\nEFGH")) #{"Apache-2.0"}))
     (is (= (licenses-within-text (str "ABCD\n" @commonmark-java-license "\nEFGH")) #{"BSD-2-Clause"})))
   (testing "Matching larger texts with multiple licenses and (optionally) other text (e.g. exceptions) that shouldn't match"
